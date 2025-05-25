690339cf44b3ca7ce7d8d2ec95064d63433ab4a7
Hoàng Anh Phạm <hoanganhpham.rasta@gmail.com>
2025-05-25 22:14:25 +0700

    Rresource Server And Client Applications

---
+++ WebApi.ResourceServer/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.ResourceServer.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [StringLength(500)]
+        public string Description { get; set; }
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+    }
+}
---
+++ WebApi.ResourceServer/Program.cs
@@ -0,0 +1,89 @@
+﻿
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace WebApi.ResourceServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+          
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddControllers()
+              .AddJsonOptions(options =>
+              {
+                  // This will use the property names as defined in the C# model
+                  options.JsonSerializerOptions.PropertyNamingPolicy = null;
+              });
+
+
+            // Cấu hình Authentication Middleware để sử dụng JWT Bearer
+            //builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            //    .AddJwtBearer(options =>
+            //    {
+            //        options.TokenValidationParameters = new TokenValidationParameters
+            //        {
+            //            ValidateIssuer = true,
+            //            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            //            ValidateAudience = false,
+            //            ValidateLifetime = true,
+            //            ValidateIssuerSigningKey = true,
+            //            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]))
+            //        };
+            //    });
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(options =>
+              {
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      ValidateIssuer = true,
+                      ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                      ValidateAudience = false,
+                      ValidateLifetime = true,
+                      ValidateIssuerSigningKey = true,
+                      IssuerSigningKeyResolver = (token, securityToken, kid, validationParameters) =>
+                      {
+                          var httpClient = new HttpClient();
+                          var jwks = httpClient.GetStringAsync(builder.Configuration["Jwt:JWKS"]).Result;
+                          var keys = new JsonWebKeySet(jwks).Keys;
+                          return keys;
+                      }
+                  };
+              });
+
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
